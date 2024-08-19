66449d8f48879677ae55d282cd51c974d30ac0ad
SamuelWyck <samawyck@gmail.com>
2024-08-19 12:27:08 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const project_divs = document.querySelectorAll(".col-4")
     project_divs.forEach(function(project_div) {
         project_div.onclick  = function() {
-            window.location.href = `projects/project_page/${project_div.dataset.projectId}`
+            window.location.href = `project_page/${project_div.dataset.projectName}.html`
         }
     })
     document.querySelector("#project-link").style.display = "none"
